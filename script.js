3e674c186256245f956c45d45394d9550dd2143a
Dmitry Drigota <drigota@gmail.com>
2023-11-27 17:28:47 +0000

    refactor: separate create post and render initial posts

--- script.js
+++ script.js
@@ -38,58 +38,76 @@ function handleFormSubmit(evt) {
 handleEditPopup();
 formElement.addEventListener('submit', handleFormSubmit);
 
-// карточки при загрузке
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
-
 const postsContainerElement = document.querySelector('.posts-grid__list');
 const postTemplate = document.querySelector('#post-template').content;
 
+// создание новго поста
+function createPostElement(card) {
+  const postElement = postTemplate
+    .querySelector('.posts-grid__list-item')
+    .cloneNode(true);
+
+  const postImage = postElement.querySelector('.post__image');
+  const postText = postElement.querySelector('.post__text');
+
+  postImage.src = card.link;
+  postText.textContent = card.name;
+
+  handleLike(postElement);
+  handleDelete(postElement);
+
+  return postElement;
+}
+
 function renderInitialPosts() {
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    },
+  ];
+
   for (let i = 0; i < initialCards.length; i++) {
-    const postElement = postTemplate
-      .querySelector('.posts-grid__list-item')
-      .cloneNode(true);
-
-    const postImage = postElement.querySelector('.post__image');
-    const postText = postElement.querySelector('.post__text');
-
-    postImage.src = initialCards[i].link;
-    postText.textContent = initialCards[i].name;
-    handleLike(postElement);
+    const postElement = createPostElement(initialCards[i]);
     postsContainerElement.append(postElement);
   }
 }
 
+// обработка лайков, которая потом загружается в функции отрисовки постов
 function handleLike(postElement) {
   postElement
     .querySelector('.post__like-button')
     .addEventListener('click', (evt) => {
       evt.target.classList.toggle('post__like-button_liked');
+    });
+}
+
+// обработка удаления постов, которая потом загружается в функции отрисовки постов
+function handleDelete(postElement) {
+  postElement
+    .querySelector('.posts-grid__delete-button')
+    .addEventListener('click', (evt) => {
+      evt.target.closest('.posts-grid__list-item').remove();
     });
 }
 
