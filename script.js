2c3669da46cda4b688a96217339ada12653987a8
Dmitry Drigota <drigota@gmail.com>
2023-11-27 17:42:35 +0000

    hotfix: edit popup refactor:

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ const displayName = profileContainer.querySelector('.profile__name');
 const displayBio = profileContainer.querySelector('.profile__bio');
 const profileEditButton = document.querySelector('.profile__edit-button');
 
-// Элементы формы
-const popupElement = document.querySelector('.popup');
-const popupCloseButton = popupElement.querySelector('.popup__close-button');
-const formElement = popupElement.querySelector('.form');
-const nameInput = popupElement.querySelector('#name');
-const bioInput = popupElement.querySelector('#bio');
+// Элементы формы редактирования
+const popupEditElement = document.querySelector('#popup-edit');
+const popupCloseButton = popupEditElement.querySelector('.popup__close-button');
+const formElement = popupEditElement.querySelector('.form');
+const nameInput = popupEditElement.querySelector('#name');
+const bioInput = popupEditElement.querySelector('#bio');
 
 // открыть и закрыть модальное окно
-function togglePopup() {
-  popupElement.classList.toggle('popup_opened');
+function toggleEditPopup() {
+  popupEditElement.classList.toggle('popup_opened');
 }
 
 function handleEditPopup() {
-  profileEditButton.addEventListener('click', togglePopup);
-  popupCloseButton.addEventListener('click', togglePopup);
+  profileEditButton.addEventListener('click', toggleEditPopup);
+  popupCloseButton.addEventListener('click', toggleEditPopup);
 
   // заполнить поля текущими данными
   nameInput.value = displayName.textContent;
@@ -32,7 +32,7 @@ function handleFormSubmit(evt) {
   const bio = bioInput.value;
   displayName.textContent = name;
   displayBio.textContent = bio;
-  togglePopup();
+  toggleEditPopup();
 }
 
 handleEditPopup();
