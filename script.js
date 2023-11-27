0059c617e46e954fe66f3389ff8fd7c71b0079a1
Dmitry Drigota <drigota@gmail.com>
2023-11-27 16:04:19 +0000

    add edit popup handler

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-// открытие модального окна
-const profileContainerElement = document.querySelector('.profile__text');
-const displayName = profileContainerElement.querySelector('.profile__name');
-const displayBio = profileContainerElement.querySelector('.profile__bio');
+// Элементы профиля
+const profileContainer = document.querySelector('.profile__text');
+const displayName = profileContainer.querySelector('.profile__name');
+const displayBio = profileContainer.querySelector('.profile__bio');
+const profileEditButton = document.querySelector('.profile__edit-button');
 
-function handlePopup() {
-  const profileEditButton = document.querySelector('.profile__edit-button');
-  const popupElement = document.querySelector('.popup');
-  const popupCloseButton = popupElement.querySelector('.popup__close-button');
-  const nameForm = popupElement.querySelector('#name');
-  const bioForm = popupElement.querySelector('#bio');
+// Элементы формы редактирования профиля
+const popupElement = document.querySelector('.popup');
+const popupCloseButton = popupElement.querySelector('.popup__close-button');
+const formElement = popupElement.querySelector('.form');
+const nameInput = popupElement.querySelector('#name');
+const bioInput = popupElement.querySelector('#bio');
 
-  profileEditButton.addEventListener('click', () => {
-    popupElement.classList.toggle('popup_opened');
-  });
-  popupCloseButton.addEventListener('click', () => {
-    popupElement.classList.toggle('popup_opened');
-  });
-
-  nameForm.value = displayName.textContent;
-  bioForm.value = displayBio.textContent;
+// открыть и закрыть модальное окно
+function togglePopup() {
+  popupElement.classList.toggle('popup_opened');
 }
 
-handlePopup();
+function handleEditPopup() {
+  profileEditButton.addEventListener('click', togglePopup);
+  popupCloseButton.addEventListener('click', togglePopup);
+
+  // заполнить поля текущими данными
+  nameInput.value = displayName.textContent;
+  bioInput.value = displayBio.textContent;
+}
+
+// отправка формы изменения персональных данных
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  const name = nameInput.value;
+  const bio = bioInput.value;
+  displayName.textContent = name;
+  displayBio.textContent = bio;
+  togglePopup();
+}
+
+handleEditPopup();
+formElement.addEventListener('submit', handleFormSubmit);
