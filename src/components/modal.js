3b77b32807e8c3410963b56c4167ddc9bb04645c
Dmitry Drigota <drigota@gmail.com>
2023-12-06 13:57:18 +0000

    minor import fixes

--- src/components/modal.js
+++ src/components/modal.js
@@ -11,25 +11,6 @@ import {
 
 import { openPopup, closePopup } from './utils.js';
 
-// закрыть модальное окно по клику на оверлей или на кнопку закрытия
-function handlePopupClick(evt) {
-  const currentPopupElement = document.querySelector('.popup_opened');
-  if (
-    evt.target.classList.contains('popup') ||
-    evt.target.classList.contains('popup__close-button')
-  ) {
-    closePopup(currentPopupElement);
-  }
-}
-
-// закрыть модальное окно по нажатию на Esc
-function handlePopupEsc(evt) {
-  if (evt.key === 'Escape') {
-    const currentPopupElement = document.querySelector('.popup_opened');
-    closePopup(currentPopupElement);
-  }
-}
-
 // взаимодействие с формой редактирования профиля
 function handleEditPopup() {
   profileEditButton.addEventListener('click', () => {
@@ -55,10 +36,4 @@ function handleAddPopup() {
   });
 }
 
-export {
-  handlePopupClick,
-  handlePopupEsc,
-  handleEditPopup,
-  handleEditFormSubmit,
-  handleAddPopup,
-};
+export { handleEditPopup, handleEditFormSubmit, handleAddPopup };
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,4 +1,21 @@
-import { handlePopupEsc, handlePopupClick } from './modal.js';
+// закрыть модальное окно по клику на оверлей или на кнопку закрытия
+function handlePopupClick(evt) {
+  const currentPopupElement = document.querySelector('.popup_opened');
+  if (
+    evt.target.classList.contains('popup') ||
+    evt.target.classList.contains('popup__close-button')
+  ) {
+    closePopup(currentPopupElement);
+  }
+}
+
+// закрыть модальное окно по нажатию на Esc
+function handlePopupEsc(evt) {
+  if (evt.key === 'Escape') {
+    const currentPopupElement = document.querySelector('.popup_opened');
+    closePopup(currentPopupElement);
+  }
+}
 
 // открыть модальное окно
 export function openPopup(popupElement) {
